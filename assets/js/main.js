0f54b6c2e5f118698d85748c556f1fc6858edbf4
Phoebe Chen <phoebe_chen@dlsu.edu.ph>
2019-09-09 23:55:38 +0800

    landing page and assets

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.single-item').slick({
+       infinite: true,
+	dots: true,
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	fade: true,
+	fadeSpeed: 1000
+
+  });//carousel
+
+ $(function() {
+     $('audio').audioPlayer();
+ });
+
+})
