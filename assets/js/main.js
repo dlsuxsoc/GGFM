16f1997c9b8dafbb29eb6051ec30f110d3ec0a04
Phoebe Chen <phoebe_chen@dlsu.edu.ph>
2019-09-27 17:29:41 +0800

    filter dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,37 @@ $(document).ready(function(){
 	autoplaySpeed: 3000,
 	fade: true,
 	fadeSpeed: 1000
-
+    
+    
   });//carousel
  
  $(function() {
      $('audio').audioPlayer()
  });
     
+    
+$(".dropdown").hide();
+
 $('.demo').slick({
   infinite: true,
   slidesToScroll: 1,
   slidesPerRow: 3,
     arrows: true
 });
+    
+$("body").click((e)=>{
+   if($(e.target).hasClass("btndropdown")){
+        if($(".dropdown").is(":hidden")){
+            $(".dropdown").slideDown("slow");
+        }else{
+              $(".dropdown").slideUp("slow");
+        }
+    }
+    else{
+        if($(".dropdown").is(":visible")){
+            $(".dropdown").slideUp("slow");
+        }      
+    }
+})
 		
 })
