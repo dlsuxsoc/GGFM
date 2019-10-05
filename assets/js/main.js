b934aba27926042b45f3f3244c5abb402eafe296
unknown <phoebe_chen@dlsu.edu.ph>
2019-10-05 21:21:55 +0800

    sliders

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,32 +22,45 @@ $('.demo').slick({
   slidesPerRow: 3,
     arrows: true
 });
+
+var slider = $(".blog-carousel");
     
-  $('.blog-carousel').slick({
-       infinite: true,
-	dots: false,
-	arrows: false,
-	autoplay: true,
-	autoplaySpeed: 3000,
-	fade: true,
-	fadeSpeed: 1000,
-  });//carousel
- 
+slider
+  .slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+      slidesToScroll: 1,
+      slidesPerRow:1
+  });
     
-$("body").click((e)=>{
-   if($(e.target).hasClass("btndropdown")){
-        if($(".dropdown").is(":hidden")){
-            $(".dropdown").slideDown("slow");
-        }else{
-              $(".dropdown").slideUp("slow");
-        }
-    }
-    else{
-        if($(".dropdown").is(":visible")){
-            $(".dropdown").slideUp("slow");
-        }      
-    }
+$(".blog-overlay").on('wheel', (function(e) {
+  e.preventDefault();
+
+  if (e.originalEvent.deltaX > 0) {
+    slider.slick('slickNext');
+  } else {
+    slider.slick('slickPrev');
+  }
+}));
+
+    
+//$("body").click((e)=>{
+//   if($(e.target).hasClass("btndropdown")){
+//        if($(".dropdown").is(":hidden")){
+//            $(".dropdown").slideDown("slow");
+//        }else{
+//              $(".dropdown").slideUp("slow");
+//        }
+//    }
+//    else{
+//        if($(".dropdown").is(":visible")){
+//            $(".dropdown").slideUp("slow");
+//        }      
+//    }
+//})
+    
 })
-    
-})
+
+
 
